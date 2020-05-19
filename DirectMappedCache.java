02cba8d6ef51db00d90479de00888801fe7f7947
madhur19171 <55681891+madhur19171@users.noreply.github.com>
2020-05-19 21:01:34 +0530

    Added Hit, Miss and Hit Ratio Display

--- DirectMappedCache.java
+++ DirectMappedCache.java
@@ -10,7 +10,7 @@ public class DirectMappedCache {
 
         MainMemory(int number_block) {
             this.number_block = number_block;
-            block = new int[1024][number_block];
+            block = new int[100000][number_block];
         }
 
         int[] read(String address) {
@@ -48,6 +48,8 @@ public class DirectMappedCache {
         CacheLine[] cache;
         int number_block;
         MainMemory MM;
+        int hit;
+        int miss;
 
         DirectMapped(int number_block, int tag, int cache_lines) {
             super(number_block, tag);
@@ -56,12 +58,14 @@ public class DirectMappedCache {
             for (int i = 0; i < cache_lines; i++)
                 cache[i] = new CacheLine(number_block, tag);
             MM = new MainMemory(number_block);
+            hit = 0;
+            miss = 0;
         }
 
         int[] breakAddress(String address) {
             int olength = (int) (Math.log(number_block) / Math.log(2));  //3
-            int ilength = (int) Math.ceil(Math.log(cache.length) / Math.log(2));  //4
-            int tlength = address.length() - olength - ilength; // 25
+            int ilength = (int) Math.ceil(Math.log(cache.length) / Math.log(2));  //6
+            int tlength = address.length() - olength - ilength; // 23
             int offset = Integer.parseInt(address.substring(address.length() - olength), 2);
             int index = Integer.parseInt(address.substring(tlength, address.length() - olength), 2);
             int tag = Integer.parseInt(address.substring(0, tlength), 2);
@@ -75,12 +79,16 @@ public class DirectMappedCache {
             int offset = breakAddress[2];
             CacheLine line = cache[index];
             if (line.tag != tag) {
-                System.out.println("Read Miss");
+                System.out.print("Read Miss ");
+                miss++;
                 //Bring Data From Main Memory
                 //Write It To Cache
                 line.tag = tag;
                 line.block = MM.read(address);
                 //Read It.
+            } else {
+                System.out.print("Read Hit ");
+                hit++;
             }
             return line.read(offset);
         }
@@ -94,11 +102,15 @@ public class DirectMappedCache {
             CacheLine line = cache[index];
             if (line.tag != tag) {
                 System.out.println("Write Miss");
+                miss++;
                 //Bring Data From Main Memory
                 //Write It To Cache
                 line.tag = tag;
                 line.block = MM.read(address);
                 //Overwrite data onto it.
+            } else {
+                System.out.println("Write Hit");
+                hit++;
             }
             line.write(data, offset);
             //Write Through
@@ -110,6 +122,7 @@ public class DirectMappedCache {
             int ind = 0;
             for (CacheLine cl : cache)
                 System.out.println(ind++ + "\t\t" + cl.tag + "\t\t" + Arrays.toString(cl.block));
+            System.out.println("Hits - " + hit + "\tMiss - " + miss + "\tHit Ratio - " + (1.0 * hit / (hit + miss)));
             System.out.println();
         }
     }
@@ -121,25 +134,26 @@ public class DirectMappedCache {
         System.out.println("Enter The Block Size");
         int B = kb.nextInt();
         kb.nextLine();
-        DirectMapped DM = new DirectMapped(B, 0, CL);
+        DirectMapped DM = new DirectMapped(B, -1, CL);
         System.out.println("Enter Queries");
         outer:
         while (true) {
-            String str = kb.next();
+            String str = kb.nextLine();
             switch (str.toUpperCase().charAt(0)) {
                 case 'E':
                     break outer;
                 case 'R':
-                    DM.read(kb.nextLine());
+                    System.out.println(DM.read(str.substring(str.indexOf(' ') + 1)));
                     break;
                 case 'W':
-                    String addr = kb.next();
-                    DM.write(kb.nextInt(), addr);
+                    String addr = str.substring(str.indexOf(' ') + 1, str.lastIndexOf(' '));
+                    int data = Integer.parseInt(str.substring(str.lastIndexOf(' ') + 1));
+                    DM.write(data, addr);
                     break;
                 case 'T':
-                    String source = kb.next();
-                    String destination = kb.next();
-                    int data = DM.read(source);
+                    String source = str.substring(str.indexOf(' ') + 1, str.lastIndexOf(' '));
+                    String destination = str.substring(str.lastIndexOf(' ') + 1);
+                    data = DM.read(source);
                     DM.write(data, destination);
                     break;
                 case 'P':
