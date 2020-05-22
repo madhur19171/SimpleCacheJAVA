fe989dfbe5b4b8bed99cbabab0a490171daaa3c2
madhur19171 <55681891+madhur19171@users.noreply.github.com>
2020-05-22 16:50:07 +0530

    Added Documentation

--- DirectMappedCache.java
+++ DirectMappedCache.java
@@ -3,7 +3,17 @@ package COAssignment;
 import java.util.*;
 
 public class DirectMappedCache {
-
+    /**
+     * Creating a main memory of size 100000*number_of_blocks*4 bytes.
+     * this memory is word indexed, that is each address can supply a unique word.
+     * The Memory sends out data in blocks of word. The number of words in each block
+     * are determined by the variable number_block.
+     * This function provides two functions viz. Read and Write.
+     * Read returns the entire block from which the appropriate hardware
+     * that requested the data can read the appropriate word from the returned block.
+     * Write function Takes the entire data block as the data to be written and the address
+     * where the content has to be written.
+     */
     static class MainMemory {
         int[][] block;
         int number_block;
@@ -26,6 +36,18 @@ public class DirectMappedCache {
         }
     }
 
+    /**
+     * The CacheLine class is the heart of all the caches.
+     * The CacheLine is implemented just like an actual cache
+     * would work on a hardware level.
+     * It consists of a tag and a data block as its attributes/
+     * The tag uniquely Identifies the CacheLine and data stores the data/
+     * It has two functions viz. Read and Write.
+     * The Read function takes the block number to be read as the input.
+     * It returns the data in that location of the block as the output.
+     * The Write function takes the data as its input to be written and the
+     * block number to where it is to be written. It returns nothing.
+     */
     static class CacheLine {
         int[] block;
         int tag;
@@ -44,6 +66,13 @@ public class DirectMappedCache {
         }
     }
 
+    /**
+     * The DirectMapped class mainly brings together different pieces.
+     * It has an array of CacheLines just like a real cache.
+     * Each cache line can be addressed by a unique address to it.
+     * This class initiates a Main Memory and a CacheLine Array.
+     * It also has two variables viz hit and miss to count the number of his and misses.
+     */
     static class DirectMapped extends CacheLine {
         CacheLine[] cache;
         int number_block;
@@ -62,6 +91,18 @@ public class DirectMappedCache {
             miss = 0;
         }
 
+        /**
+         * This method is used to break the address into its appropriate components
+         * and return those components.
+         * The address is broken int 3 parts:
+         * Offset: This determines the position of the required word inside the block.
+         * Index: It is to index the appropriate CacheLine. This is key to the Direct Mapped Cache.
+         * Tag: It is the part of the address which is compared with the Tag of the CacheLine to determine
+         * if the cache line is the one that is required or not.
+         *
+         * @param address This  parameter  is the address which has to be broken
+         * @return int[]
+         */
         int[] breakAddress(String address) {
             int olength = (int) (Math.log(number_block) / Math.log(2));  //3
             int ilength = (int) Math.ceil(Math.log(cache.length) / Math.log(2));  //6
@@ -72,6 +113,19 @@ public class DirectMappedCache {
             return new int[]{tag, index, offset};
         }
 
+        /**
+         * This function is to read the content at the address in the cache.
+         * There are two possibilities, either the data is in cache or not.
+         * If it is in the cache, the tag matches with the indexed cache line
+         * then the read function of the cache line is called and the value is returned.
+         * If the required block is not in the cache, a miss is generated.
+         * The tag is set as the tag to be read in the appropriate indexed cache line.
+         * The block is read from the main memory and written into the appropriate cache line.
+         * Then the block is again read from the cache. This time a hit occurs.
+         *
+         * @param address This  parameter  is the address which has to be Read
+         * @return int
+         */
         int read(String address) {
             int[] breakAddress = breakAddress(address);
             int tag = breakAddress[0];
@@ -93,6 +147,19 @@ public class DirectMappedCache {
             return line.read(offset);
         }
 
+        /**
+         * This function is to Write the content at the address in the cache.
+         * There are two possibilities, either the block is in cache or not.
+         * If it is in the cache, the tag matches with the indexed cache line
+         * then the write function of the cache line is called and the Data is written.
+         * If the required block is not in the cache, a miss is generated.
+         * The tag is set as the tag to be read in the appropriate indexed cache line.
+         * The block is read from the main memory and written into the appropriate cache line.
+         * Then the block is again written in the cache. This time a hit occurs.
+         *
+         * @param data    Data to be written (Integer)
+         * @param address Address where the data is to be written.
+         */
         void write(int data, String address) {
             int[] breakAddress = breakAddress(address);
             int tag = breakAddress[0];
@@ -106,6 +173,7 @@ public class DirectMappedCache {
                 //Bring Data From Main Memory
                 //Write It To Cache
                 line.tag = tag;
+                //Not really necessary, but just to maintain fluency.
                 line.block = MM.read(address);
                 //Overwrite data onto it.
             } else {
@@ -117,6 +185,9 @@ public class DirectMappedCache {
             MM.write(line.block, address);
         }
 
+        /**
+         * Prints the current contents of the cache
+         */
         void print() {
             System.out.println("Line\tTag\t\tData");
             int ind = 0;
@@ -127,6 +198,18 @@ public class DirectMappedCache {
         }
     }
 
+    /**
+     * The Main method takes number of Cache Lines to be in the cache
+     * and the number of blocks in each cache lines as the inputs.
+     * Then it takes Queries in the form:
+     * R <Address>
+     * W <Address> <Data>
+     * T <Source Address> <Destination Address>
+     * P is to print the contents of the cache.
+     * E is to exit the queries and stop the execution.
+     *
+     * @param args IDK
+     */
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         System.out.println("Enter The Number of Cache Lines");
